779a323f75ac43ff4625b67e7fbafa7195837f94
Atharv Lingayat <76042372+atharv2608@users.noreply.github.com>
2023-06-10 11:16:07 +0530

    jsx error fixed

--- o2react/src/App.js
+++ o2react/src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Login from './Pages/Login';
 import Registration from './Pages/Registration';
+import React, { Component }  from 'react';
 
 function App() {
   return (
