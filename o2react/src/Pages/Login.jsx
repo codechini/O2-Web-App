f99ad357219410582b890e75e5223429631cf236
codechini <arangathvenuchinmay@gmail.com>
2023-07-21 20:00:04 +0530

    managed_log_reg

--- o2react/src/Pages/Login.jsx
+++ o2react/src/Pages/Login.jsx
@@ -1,62 +1,34 @@
-import React from 'react'
-import { Button, Col, Container, Form, Modal, ModalFooter, Row } from 'react-bootstrap'
-import { FcGoogle } from 'react-icons/fc'
+import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
 const Login = () => {
+  function greet() {
+    console.log("Howdy mate");
+  }
   return (
     <>
-      <Modal
-        size="sm"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={true}
-      >
-        <Container>
-          <Modal.Body>
-            <Form className='text-center px-0 pt-4'> {/*onSubmit={Function()} */}
-              <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
-                <Form.Label className='user-sel'>
-                  Email address
-                </Form.Label>
-                <Form.Control type="email" placeholder="Enter email" id='email' />
-              </Form.Group>
+      <div className="container">
+        <div className="outer">
+          <div className="inner">
+            <p className='header'>Login</p>
+            <form action="" method="get" onSubmit={greet}>
+              <input type="text" placeholder='Name' id='uname' autoComplete='username' />
+              <input type="email" placeholder='Email' id='email' autoComplete='username email' pattern='"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"' />
+              <input type="password" placeholder='Password' id='pass' autocomplete="current-password" />
+              <input type="submit" value="submit" />
+            </form>
+            <span>OR</span>
+            <div className="container">
+              <div className="login-w-google">
+                <a href="https://ghchart.rshah.org/">
+                  <FcGoogle size={26} />&nbsp;&nbsp;Login with Google
+                </a>
+              </div>
+            </div>
 
-              <Form.Group className="mb-3 text-start" controlId="formBasicEmail">
-                <Form.Label className='user-sel'>
-                  Password
-                </Form.Label>
-                <Form.Control type="password" placeholder="Password" id='pass' />
-              </Form.Group>
+          </div>
+        </div>
+      </div>
 
-              <Button variant="outline-success" type="submit">
-                Login
-              </Button>
-
-              <Form.Group className="mt-2 text-center">
-                <Form.Label className='user-sel'>OR</Form.Label>
-              </Form.Group>
-
-              <Row className='justify-content-md-center px-5'>
-                <Col>
-                  <a className='d-flex justify-content-center' href="#">    {/*Update to Link tag --react */}
-                    <FcGoogle size={26} />
-                  </a>
-                </Col>
-                <Col>
-                  <a className='d-flex justify-content-center' href="#">    {/*Update to Link tag --react */}
-                    <FcGoogle size={26} />
-                  </a>
-                </Col>
-                <Col xs>
-                  <a className='d-flex justify-content-center' href="#">    {/*Update to Link tag --react */}
-                    <FcGoogle size={26} />
-                  </a>
-                </Col>
-              </Row>
-            </Form>
-
-          </Modal.Body>
-        </Container>
-      </Modal>
     </>
   )
 }
